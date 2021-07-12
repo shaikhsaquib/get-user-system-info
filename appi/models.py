ae3ed3b284dd5df8bf3c9b74b680fbb4fa6791de
shaikh saquib <shaikhsaquib297@gmail.com>
2021-07-12 20:38:03 +0530

    sucess

---
+++ appi/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+class data(models.Model):
+    id=models.IntegerField(primary_key=True)
+    hostname=models.CharField(max_length=50)
+    ip_address=models.GenericIPAddressField()
+    date_time=models.TimeField(auto_now=False, auto_now_add=False,)
+    n=models.IntegerField()
+    def __str__(self):
+        return self.hostname
---
+++ appi/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+import socket
+import datetime
+import random
+from .models import data
+# Create your views here.
+def index(request):
+    if request.method=="GET":
+        hostname = socket.gethostname()
+        ip_address = socket.gethostbyname(hostname)
+        date_time=datetime.datetime.now()
+        n = random.randint(20,50)
+        deta=data(hostname=hostname, ip_address= ip_address,date_time=date_time,n=n)
+    deta.save()
+    context={"hostname":hostname,"ip_address":ip_address,"date_time":date_time,"n":n}
+    return render(request, 'index.html',context)
+
+def show(request):
+    if request.method=="GET":
+        de=data.objects.all()
+        context={"deta":de}
+    return render(request,'show.html',context)
