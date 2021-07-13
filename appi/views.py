f218f4f4eb9483c954abc163bdc5c05822557830
shaikh saquib <shaikhsaquib297@gmail.com>
2021-07-13 16:17:49 +0530

    commit

--- appi/views.py
+++ appi/views.py
@@ -11,7 +11,7 @@ def index(request):
         date_time=datetime.datetime.now()
         n = random.randint(20,50)
         deta=data(hostname=hostname, ip_address= ip_address,date_time=date_time,n=n)
-    # deta.save()
+        deta.save()
     context={"hostname":hostname,"ip_address":ip_address,"date_time":date_time,"n":n}
     return render(request, 'index.html',context)
 
