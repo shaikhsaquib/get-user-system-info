d596ad67c1bafa516d14fa3ee040e1fea8aa9a9e
shaikh saquib <shaikhsaquib297@gmail.com>
2021-07-13 16:44:05 +0530

    commit

--- appi/views.py
+++ appi/views.py
@@ -11,13 +11,12 @@ def index(request):
         date_time=datetime.datetime.now()
         n = random.randint(20,50)
         deta=data(hostname=hostname, ip_address= ip_address,date_time=date_time,n=n)
-        # deta.save()
+        deta.save()
     context={"hostname":hostname,"ip_address":ip_address,"date_time":date_time,"n":n}
     return render(request, 'index.html',context)
 
 def show(request):
-    # if request.method=="GET":
-        # de=data.objects.all()
-        # context={"deta":de}
-    return render(request,'show.html')
-    # ,context)
+    if request.method=="GET":
+        de=data.objects.all()
+        context={"deta":de}
+    return render(request,'show.html',context)
